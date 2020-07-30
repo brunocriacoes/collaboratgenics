19677d60be505ff058d5633626ad19caee4ae796
brunocriacoes <br.rafael@outlook.com>
2020-07-30 10:06:22 -0300

    fix

--- vendor/zendframework/zend-diactoros/src/functions/create_uploaded_file.php
+++ vendor/zendframework/zend-diactoros/src/functions/create_uploaded_file.php
@@ -16,7 +16,7 @@ namespace Zend\Diactoros;
  * @throws Exception\InvalidArgumentException if one or more of the tmp_name,
  *     size, or error keys are missing from $spec.
  */
-function createUploadedFile(array $spec) 
+function createUploadedFile(array $spec) : UploadedFile
 {
     if (! isset($spec['tmp_name'])
         || ! isset($spec['size'])
